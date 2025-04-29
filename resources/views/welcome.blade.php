110f1992b5aee919fe65db35c4f429cf0a52db57
rasyidinazhari <wisnurasyidinazhari99@gmail.com>
2025-04-29 14:04:21 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Winninews | Platform berita jurnalistik ter-update di indonesia</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
